1ce3ccf274b96c58e294e2ad990f47d2a53b5909
Nulu <Nnabwami@gmail.com>
2016-10-17 19:51:08 -0500

    Task 1 completed

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 // GO!
-// var toggleButton = document.querrySelector("#toggleButton"); 
-// var onB = document.querySelector(); 
-// var offB = document.querySelector();
 
+// grab toggleButton
+var toggleButton = document.querySelector('#toggleButton')
 
+// store navMenuState
+var navMenuState = {
+	hidden: false
+}
 
+// 
+var toggleNavMenu = function() {
+	// grab nav-menu
+	var navMenu = document.querySelector('.nav-menu')
+	// grab toggleButton
+	var toggleButton = document.querySelector('#toggleButton')
 
-// toggleButton.addEventListener("click", function() {
-//     // toggleButtons(1, mainButton, "body");
-//     alert("kjljljlks")
-// });
+    // check current states if hidden show nav-menu
+    if (navMenuState.hidden) {
 
+      navMenu.style.opacity = '1'
+      toggleButton.value="hide nav"
+      navMenuState.hidden = false
+    
+	// or else hide nav-menu
+    }else {
 
-// grab teacher button
-var tchrButtonEl = document.querySelector('#toggleButton')
-
-// write the event handler: the function that broadcasts the alert
-var alerter = function() {
+    // change opacity to zero inorder to hide menu
+      navMenu.style.opacity = '0'
+      // change button text displayed
+      toggleButton.value="show nav"
+      // store current status
+      navMenuState.hidden = true
+    
+    }
   
 }
 
-// tell the teacher button to run the function when it is clicked
-tchrButtonEl.addEventListener('click', alerter)
+// tell the toggle button to run the toggleNaveMenu fuction when its clicked
+toggleButton.addEventListener('click', toggleNavMenu)
