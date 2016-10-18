a54327b42a5b2300663ef682517fb1720a081355
Nulu <Nnabwami@gmail.com>
2016-10-17 19:11:06 -0500

    Task#1

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 // GO!
+// var toggleButton = document.querrySelector("#toggleButton"); 
+// var onB = document.querySelector(); 
+// var offB = document.querySelector();
 
+
+
+
+// toggleButton.addEventListener("click", function() {
+//     // toggleButtons(1, mainButton, "body");
+//     alert("kjljljlks")
+// });
+
+
+// grab teacher button
+var tchrButtonEl = document.querySelector('#toggleButton')
+
+// write the event handler: the function that broadcasts the alert
+var alerter = function() {
+  
+}
+
+// tell the teacher button to run the function when it is clicked
+tchrButtonEl.addEventListener('click', alerter)
