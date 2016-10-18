caf00de87db736eff43b39150bb22a837f2a5a42
Nulu <Nnabwami@gmail.com>
2016-10-17 20:31:09 -0500

    Completing Task #2

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // GO!
-
+var KEYCODES = {
+  enter: 13
+}
+// TASK #1
 // grab toggleButton
 var toggleButton = document.querySelector('#toggleButton')
 
@@ -38,3 +41,26 @@ var toggleNavMenu = function() {
 
 // tell the toggle button to run the toggleNaveMenu fuction when its clicked
 toggleButton.addEventListener('click', toggleNavMenu)
+
+
+// Task #2
+// grab input text 
+var inputEl = document.querySelector('#add-guest input[type="text"]')
+
+var itemHandler = function(keyEvent) {
+	var elmentList = document.querySelector("#add-guest .guest-list")
+
+  // determine whether the enter key was pressed
+  if (keyEvent.keyCode === KEYCODES.enter) {
+    var inputEl = keyEvent.target,
+        itemString = inputEl.value
+
+    var node = document.createElement("LI"); 
+    var textnode = document.createTextNode(itemString) 
+    node.appendChild(textnode);
+    elmentList.appendChild(node);
+    // elmentList.addChild append
+  }
+}
+
+inputEl.addEventListener('keydown',itemHandler)
